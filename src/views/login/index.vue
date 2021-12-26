f24b4253c4bfe84546a3a1cbb02d205bafaec433
liushihong <liushihong21@126.com>
2021-12-26 21:32:09 +0800

    axios 跨域配置

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,9 +5,7 @@
         <div class="col-12 col-sm-9 col-md-7 col-lg-5 m-auto pt-5">
           <div class="card mt-5">
             <div class="card-header bg-white">
-              <h3 class="text-center mb-0 text-secondary">
-                UNI-ADMIN
-              </h3>
+              <h3 class="text-center mb-0 text-secondary">UNI-ADMIN</h3>
             </div>
             <div class="card-body">
               <el-form ref="form" :rules="rules" :model="form">
@@ -48,33 +46,44 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       form: {
         username: "",
-        password: ""
+        password: "",
       },
       rules: {
         username: [
           {
             required: true,
             message: "请输入用户名",
-            trigger: "blur"
-          }
+            trigger: "blur",
+          },
         ],
-        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
-      }
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
     };
   },
   methods: {
     handleSubmit(formName) {
-      this.$refs[formName].validate(valid => {
+      this.$refs[formName].validate((valid) => {
         if (!valid) return;
-        this.$router.push({ name: "index" });
+        //提交表单  // 用户名：admin 密码：admin
+        axios
+          .post("/admin/login", this.form)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        // this.$router.push({ name: "index" });
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
