19363066dd5269df2e6c67caf19d2280ceca65f6
liushihong <liushihong21@126.com>
2022-01-01 18:58:03 +0800

    全局路由守卫-登录验证-1

--- src/router.js
+++ src/router.js
@@ -5,4 +5,29 @@ import routes from "./common/config/router.js"
 
 Vue.use(Router)
 
-export default new Router({ routes })
+
+let router = new Router({
+    routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+    let token = window.sessionStorage.getItem("token");
+    // 已登录
+    if (token) {
+        next(); //放行
+    } else {
+        // 跳转到登录页验证
+        if (to.path === "/login") {
+            return next();
+        }
+        // 未登录
+        Vue.prototype.$message.error("请先登录！")
+        next("/login");
+
+    }
+
+})
+
+
+export default router;
